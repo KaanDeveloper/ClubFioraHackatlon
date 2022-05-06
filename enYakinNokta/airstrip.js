e9f5804e70e351d471de2eb7029e5ddc05cb2b85
KaanDeveloper <kaanrt02@gmail.com>
2022-05-07 02:27:58 +0300

    BugFix

--- enYakinNokta/airstrip.js
+++ enYakinNokta/airstrip.js
@@ -1,4 +1,4 @@
-const airstripCoordinates = [[10.41, 0,21], [50.41, 1.11], [30.25, 2.12], [80.65, 0.55]]
+const airstripCoordinates = [[10.41, 0.21], [50.41, 1.11], [30.25, 2.12], [80.65, 0.55]]
 const clientCoordinates = [20.12, 0.55]
 
 function closestAirstrip(airstripCoordinates, clientCoordinates) {
@@ -16,29 +16,28 @@ function closestAirstrip(airstripCoordinates, clientCoordinates) {
         result = 0
     }
     min = myArrayMin(distances)
+    
     if(distances[distances.indexOf(min)] === distances[0]) {
-        console.log("En Yakın Havaalanı A Havaalanıdır: " + Math.round(min) + " km")
+        console.log("En Yakın Havaalanı A Havaalanıdır: " + min + " km")
     }
     if(distances[distances.indexOf(min)] === distances[1]) {
-        console.log("En Yakın Havaalanı B Havaalanıdır: " + Math.round(min) + " km")
+        console.log("En Yakın Havaalanı B Havaalanıdır: " + min + " km")
     }
     if(distances[distances.indexOf(min)] === distances[2]) {
-        console.log("En Yakın Havaalanı C Havaalanıdır: " + Math.round(min) + " km")
+        console.log("En Yakın Havaalanı C Havaalanıdır: " + min + " km")
     }
     if(distances[distances.indexOf(min)] === distances[3]) {
-        console.log("En Yakın Havaalanı D Havaalanıdır: " + Math.round(min) + " km")
+        console.log("En Yakın Havaalanı D Havaalanıdır: " + min + " km")
     }
 }
 
 function myArrayMin(arr) {//This is a helper method
-    let len = arr.length;
-    let min = Infinity;
-    while (len--) {
-      if (arr[len] < min) {
-        min = arr[len];
-      }
+    let min = arr[0]
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] < min) {
+            min = arr[i]
+        }
     }
-    return min;
-  }
-
+    return min
+}
 closestAirstrip(airstripCoordinates, clientCoordinates)
